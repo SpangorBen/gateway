81a4cb7e16ae94976053a09506eefaec3f1dabdc
SpangorBen <netflixbenzzinbi@gmail.com>
2025-02-26 11:41:04 +0000

    keycloak docker compose

--- src/main/java/com/javarta/ecommerce/gateway_2/config/SecurityConfig.java
+++ src/main/java/com/javarta/ecommerce/gateway_2/config/SecurityConfig.java
@@ -14,7 +14,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authorize -> authorize.anyRequest()
+        return http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/key-users/**").permitAll()
+                        .anyRequest()
                         .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
